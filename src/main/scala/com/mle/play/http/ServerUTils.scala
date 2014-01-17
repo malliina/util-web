c61146ca1eb05767a91bf7e78186d1bedcd8e973
malliina <malliina123@gmail.com>
2014-01-17 19:50:35 +0100

    refac

--- src/main/scala/com/mle/play/http/ServerUTils.scala
+++ src/main/scala/com/mle/play/http/ServerUTils.scala
@@ -7,13 +7,15 @@ import org.jboss.netty.bootstrap.ServerBootstrap
 import org.jboss.netty.channel.Channel
 import com.mle.util.{Util, Log}
 
-/**
- * The main motivation of this class is to be able to find out
- * whether a request is made over HTTPS or not.
- */
-class RequestHelpers(serverOpt: Option[NettyServer]) extends Log {
+class ServerUtils(server: NettyServer) extends Log {
+  val httpsPort = port(_.HTTPS)
+  val httpPort = port(_.HTTP)
+  val isHttpAvailable = httpPort.isDefined
+  val isHttpsAvailable = httpsPort.isDefined
+
   /**
-   * May not work if the HTTPS port is 80 and excluded from the request's <code>host</code> member.
+   * May not work if the HTTPS port is 80 and excluded
+   * from the request's <code>host</code> member.
    *
    * @param request the incoming request
    * @return true if the request was made over HTTPS, false otherwise
@@ -21,23 +23,20 @@ class RequestHelpers(serverOpt: Option[NettyServer]) extends Log {
   def isHttps(request: RequestHeader): Boolean =
     httpsPort.exists(sslPort => request.host.contains(s":$sslPort"))
 
-  def httpsPort = port(_.HTTPS)
-
-  def httpPort = port(_.HTTP)
-
   def port(req: RequestHeader): Int =
-    (if (isHttps(req)) httpsPort else httpPort) orElse portFromHost(req) getOrElse 80
+    (if (isHttps(req)) httpsPort else httpPort) orElse ServerUtils.portFromHost(req) getOrElse 80
 
+  private def port(f: NettyServer => Option[(ServerBootstrap, Channel)]): Option[Int] =
+    for {
+      fOpt <- f(server)
+      localAddr <- Option(fOpt._2.getLocalAddress)
+    } yield localAddr.asInstanceOf[InetSocketAddress].getPort
+}
+
+object ServerUtils {
   def portFromHost(req: RequestHeader): Option[Int] = {
     val maybeSuffix = req.host.dropWhile(_ != ':')
     if (maybeSuffix.size > 1) Util.optionally[Int, NumberFormatException](maybeSuffix.tail.toInt)
     else None
   }
-
-  private def port(f: NettyServer => Option[(ServerBootstrap, Channel)]): Option[Int] =
-    for {
-      server <- serverOpt
-      serverAndChannel <- f(server)
-      localAddr <- Option(serverAndChannel._2.getLocalAddress)
-    } yield localAddr.asInstanceOf[InetSocketAddress].getPort
 }
