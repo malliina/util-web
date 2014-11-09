e85cbf00ec607a3ccde8c0ac90245e1b293ec2ac
malliina <malliina123@gmail.com>
2014-11-09 21:56:47 +0200

    1.6.11

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -11,13 +11,12 @@ object PlayBuild extends Build {
   val httpGroup = "org.apache.httpcomponents"
   val httpVersion = "4.3.5"
   val playGroup = "com.typesafe.play"
-  val playVersion = "2.3.4"
+  val playVersion = "2.3.6"
   val mleGroup = "com.github.malliina"
 
   lazy val projectSettings = Seq(
-    version := "1.6.10",
+    version := "1.6.11",
     scalaVersion := "2.11.4",
-    //    crossScalaVersions := Seq("2.10.4", "2.11.1"),
     SbtUtils.gitUserName := "malliina",
     SbtUtils.developerName := "Michael Skogberg",
     libraryDependencies ++= Seq(
@@ -25,7 +24,6 @@ object PlayBuild extends Build {
       playGroup %% "play-ws" % playVersion,
       mleGroup %% "util" % "1.5.0",
       mleGroup %% "logback-rx" % "0.1.2",
-      "com.ning" % "async-http-client" % "1.8.13",
       httpGroup % "httpclient" % httpVersion,
       httpGroup % "httpmime" % httpVersion),
     fork in Test := true,
