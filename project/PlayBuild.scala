9fb3ac920860f5dc2272f49b9bea31e8164d88e9
Michael Skogberg <michael.skogberg@futurice.com>
2015-05-27 08:19:02 +0300

    1.9.3

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -1,5 +1,6 @@
 import bintray.Plugin.bintraySettings
-import com.mle.sbtutils.{SbtProjects, SbtUtils}
+import com.mle.sbtutils.SbtProjects
+import com.mle.sbtutils.SbtUtils.{developerName, gitUserName}
 import sbt.Keys._
 import sbt._
 
@@ -7,19 +8,20 @@ import sbt._
  * The build.
  */
 object PlayBuild extends Build {
-  lazy val utilPlay = SbtProjects.mavenPublishProject("util-play").settings(projectSettings: _*)
+  lazy val utilPlay = SbtProjects.testableProject("util-play").settings(projectSettings: _*)
 
   val httpGroup = "org.apache.httpcomponents"
   val httpVersion = "4.3.5"
   val playGroup = "com.typesafe.play"
-  val playVersion = "2.3.8"
+  val playVersion = "2.3.9"
   val mleGroup = "com.github.malliina"
 
   lazy val projectSettings = bintraySettings ++ Seq(
-    version := "1.9.2",
+    version := "1.9.3",
     scalaVersion := "2.11.6",
-    SbtUtils.gitUserName := "malliina",
-    SbtUtils.developerName := "Michael Skogberg",
+    gitUserName := "malliina",
+    developerName := "Michael Skogberg",
+    organization := s"com.github.${gitUserName.value}",
     libraryDependencies ++= Seq(
       playGroup %% "play" % playVersion,
       playGroup %% "play-ws" % playVersion,
