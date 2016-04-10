dbc8c36ee5a5c6a7ee78741f3b4674c36af3c8ec
Michael Skogberg <michael.skogberg@outlook.com>
2016-04-10 22:18:21 +0300

    Fix socket onConnect/onDisconnect; 2.6.3

--- util-play/src/test/scala/tests/PlayTests.scala
+++ util-play/src/test/scala/tests/PlayTests.scala
@@ -1,9 +1,28 @@
 package tests
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
 import org.scalatest.FunSuite
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Promise}
 
 class PlayTests extends FunSuite {
   test("can run test") {
     assert(1 + 1 === 2)
+  }
+
+  test("stream") {
+    implicit val system = ActorSystem("QuickStart")
+    implicit val materializer = ActorMaterializer()
+
+    val expected = 42
+    val completion = Promise[Int]()
+    val source = Source(1 to 10)
+    val sinkWithCleanup = Sink.onComplete[Int](_ => completion.trySuccess(expected))
+    source.runWith(sinkWithCleanup)
+    val answer = Await.result(completion.future, 10.seconds)
+    assert(answer === expected)
   }
 }
