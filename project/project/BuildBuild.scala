0e4f143a74ee546b20c99a0cc14cda97ae57c693
Michael Skogberg <michael.skogberg@outlook.com>
2017-02-19 20:35:04 +0200

    Import code

--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -2,16 +2,18 @@ import sbt.Keys._
 import sbt._
 
 object BuildBuild {
-
-  val settings = sbtPlugins ++ Seq(
-    Keys.scalaVersion := "2.10.6",
+  lazy val settings = sbtPlugins ++ Seq(
+    scalaVersion := "2.10.6",
     resolvers ++= Seq(
-      Resolver.url("bintray-sbt-plugin-releases",
-        url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
-      Resolver.url("malliina bintray sbt",
-        url("https://dl.bintray.com/malliina/sbt-plugins/"))(Resolver.ivyStylePatterns)
+      ivyRepo("bintray-sbt-plugin-releases",
+        "http://dl.bintray.com/content/sbt/sbt-plugin-releases"),
+      ivyRepo("malliina bintray sbt",
+        "https://dl.bintray.com/malliina/sbt-plugins/")
     )
   )
+
+  def ivyRepo(name: String, urlString: String) =
+    Resolver.url(name, url(urlString))(Resolver.ivyStylePatterns)
 
   def sbtPlugins = Seq(
     "com.malliina" %% "sbt-utils" % "0.6.1",
--- src/main/scala/com/malliina/play/json/ValidatingCompanion.scala
+++ src/main/scala/com/malliina/play/json/ValidatingCompanion.scala
@@ -2,16 +2,18 @@ package com.malliina.play.json
 
 import play.api.libs.json._
 
-abstract class ValidatingCompanion[In, T](implicit r: Format[In]) {
-  private val reader = Reads[T](json => {
-    json.validate[In].flatMap(s => build(s)
+abstract class ValidatingCompanion[In: Format, T] {
+  private val reader = Reads[T] { json =>
+    json.validate[In].flatMap(in => build(in)
       .map[JsResult[T]](t => JsSuccess(t))
-      .getOrElse(JsError(s"Invalid input: $s")))
-  })
+      .getOrElse(JsError(invalidInputMessage(in))))
+  }
   private val writer = Writes[T](t => Json.toJson(write(t)))
   implicit val json = Format[T](reader, writer)
 
   def build(input: In): Option[T]
 
   def write(t: T): In
+
+  def invalidInputMessage(in: In): String = s"Invalid input: '$in'."
 }
