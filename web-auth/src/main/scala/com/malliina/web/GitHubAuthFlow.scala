78f4052a96e9e43ffc6024e8f93272148d33f5ae
Michael Skogberg <malliina123@gmail.com>
2022-01-08 12:06:09 +0200

    Fix GitHub auth

--- web-auth/src/main/scala/com/malliina/web/GitHubAuthFlow.scala
+++ web-auth/src/main/scala/com/malliina/web/GitHubAuthFlow.scala
@@ -2,11 +2,14 @@ package com.malliina.web
 
 import cats.effect.IO
 import com.malliina.http.{FullUrl, HttpClient}
+import com.malliina.util.AppLogger
 import com.malliina.values.Email
-import com.malliina.web.GitHubAuthFlow.staticConf
+import com.malliina.web.GitHubAuthFlow.{staticConf, log}
 import com.malliina.web.WebHeaders.Accept
 
 object GitHubAuthFlow {
+  private val log = AppLogger(getClass)
+
   def staticConf(conf: AuthConf) = StaticConf(
     "user:email",
     FullUrl.https("github.com", "/login/oauth/authorize"),
@@ -25,11 +28,14 @@ class GitHubAuthFlow(authConf: AuthConf, http: HttpClient[IO])
     requestNonce: Option[String]
   ): IO[Either[AuthError, Email]] = {
     val headers = Map(Accept -> HttpConstants.Json)
+    val postHeaders = Map(WebHeaders.ContentType -> HttpConstants.FormUrlEncoded) ++ headers
     val params = validationParams(code, redirectUrl, authConf)
     for {
-      tokens <- http.postFormAs[GitHubTokens](conf.tokenEndpoint, headers, params)
-      tokenUrl = FullUrl.https("api.github.com", s"/user/emails?access_token=${tokens.accessToken}")
-      emails <- http.getAs[Seq[GitHubEmail]](tokenUrl)
+      tokens <- http.postFormAs[GitHubTokens](conf.tokenEndpoint, params, postHeaders)
+      emails <- http.getAs[Seq[GitHubEmail]](
+        FullUrl.https("api.github.com", s"/user/emails"),
+        headers ++ Map(WebHeaders.Authorization -> s"token ${tokens.accessToken}")
+      )
     } yield emails
       .find(email => email.primary && email.verified)
       .map(_.email)
