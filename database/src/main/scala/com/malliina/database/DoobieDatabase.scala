5c3a5de3a1b32ebed804b60e0201a57eabfd0ff0
Michael Skogberg <michael.skogberg@futurice.com>
2024-01-29 21:32:22 +0200

    Fast db init

--- database/src/main/scala/com/malliina/database/DoobieDatabase.scala
+++ database/src/main/scala/com/malliina/database/DoobieDatabase.scala
@@ -10,6 +10,7 @@ import doobie.*
 import doobie.implicits.*
 import doobie.util.ExecutionContexts
 import doobie.util.log.{ExecFailure, LogEvent, ProcessingFailure, Success}
+import doobie.util.transactor.Transactor.Aux
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.output.MigrateResult
 
@@ -22,18 +23,22 @@ object DoobieDatabase:
     if conf.autoMigrate then withMigrations(conf) else default(conf)
 
   def default[F[_]: Async](conf: Conf): Resource[F, DoobieDatabase[F]] =
-    for tx <- transactor(config(conf))
+    for tx <- poolTransactor(poolConfig(conf))
     yield DoobieDatabase(tx)
+
+  def fast[F[_]: Async](conf: Conf): DoobieDatabase[F] = DoobieDatabase(noPoolTransactor(conf))
 
   private def withMigrations[F[_]: Async](conf: Conf): Resource[F, DoobieDatabase[F]] =
     Resource.eval(migrate(conf)).flatMap(_ => default(conf))
 
-  private def migrate[F[_]: Sync](conf: Conf): F[MigrateResult] = Sync[F].delay {
-    val flyway = Flyway.configure.dataSource(conf.url, conf.user, conf.pass).load()
+  private def migrate[F[_]: Sync](conf: Conf): F[MigrateResult] = Sync[F].delay:
+    val flyway = Flyway.configure
+      .dataSource(conf.url, conf.user, conf.pass)
+      .table(conf.schemaTable)
+      .load()
     flyway.migrate()
-  }
 
-  private def config(conf: Conf): HikariConfig =
+  private def poolConfig(conf: Conf): HikariConfig =
     val hikari = new HikariConfig()
     hikari.setDriverClassName(Conf.MySQLDriver)
     hikari.setJdbcUrl(conf.url)
@@ -43,8 +48,7 @@ object DoobieDatabase:
     hikari.setMaximumPoolSize(conf.maxPoolSize)
     hikari
 
-  private def transactor[F[_]: Async](conf: HikariConfig): Resource[F, DataSourceTransactor[F]] =
-    val F = Async[F]
+  private def makeLogHandler[F[_]: Async]: LogHandler[F] =
     val syncLogHandler: LogEvent => Unit =
       case Success(sql, args, _, exec, processing) =>
         val logger: String => Unit = if processing > 1.seconds then log.info else log.debug
@@ -53,22 +57,34 @@ object DoobieDatabase:
         log.error(s"Failed '$sql' in ${exec + processing}.", failure)
       case ExecFailure(sql, args, _, exec, failure) =>
         log.error(s"Exec failed '$sql' in $exec.'", failure)
-    val logHandler: LogHandler[F] = event => F.delay(syncLogHandler(event))
-    val connect = F.delay {
+    event => Async[F].delay(syncLogHandler(event))
+
+  private def poolTransactor[F[_]: Async](
+    conf: HikariConfig
+  ): Resource[F, DataSourceTransactor[F]] =
+    val F = Async[F]
+    val connect = F.delay:
       log.info(
         s"Connecting to '${conf.getJdbcUrl}' with pool size ${conf.getMaximumPoolSize} as ${conf.getUsername}..."
       )
       new HikariDataSource(conf)
-    }
     val disconnect = (ds: HikariDataSource) =>
-      F.delay {
+      F.delay:
         log.info(s"Disconnecting from ${ds.getJdbcUrl} as ${conf.getUsername}...")
         ds.close()
-      }
     for
       ec <- ExecutionContexts.fixedThreadPool[F](16) // connect EC
       ds <- Resource.make(connect)(disconnect)
-    yield Transactor.fromDataSource[F](ds, ec, Option(logHandler))
+    yield Transactor.fromDataSource[F](ds, ec, Option(makeLogHandler[F]))
 
-class DoobieDatabase[F[_]: Async](tx: DataSourceTransactor[F]):
+  private def noPoolTransactor[F[_]: Async](conf: Conf): Transactor[F] =
+    Transactor.fromDriverManager[F](
+      conf.driver,
+      conf.url,
+      user = conf.user,
+      password = conf.pass,
+      logHandler = Option(makeLogHandler[F])
+    )
+
+class DoobieDatabase[F[_]: Async](tx: Transactor[F]):
   def run[T](io: ConnectionIO[T]): F[T] = io.transact(tx)
